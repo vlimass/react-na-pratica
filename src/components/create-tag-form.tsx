231f7824fb4d7f17e361a5d9309e0af0c96b79c7
Vinícius Lima <villylss@gmail.com>
2024-02-23 17:39:41 -0300

    feat: projeto de inserção de dados em formulário criado

--- src/components/create-tag-form.tsx
+++ src/components/create-tag-form.tsx
@@ -1,54 +1,45 @@
-import { Check, Loader2, X } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
+import { zodResolver } from '@hookform/resolvers/zod' 
+import { Check, X, Loader2 } from 'lucide-react'
 import { Button } from "./ui/button";
 import * as Dialog from '@radix-ui/react-dialog'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 const createTagSchema = z.object({
-  title: z.string().min(3, { message: 'Minimum 3 characters.' }),
+  title: z.string().min(3, { message: "Minimum 3 characters." })
 })
 
 type CreateTagSchema = z.infer<typeof createTagSchema>
-
-function getSlugFromString(input: string): string {
-  return  input
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .toLowerCase()
-    .replace(/[^\w\s]/g, '')
-    .replace(/\s+/g, '-');
-}
 
 export function CreateTagForm() {
   const queryClient = useQueryClient()
 
   const { register, handleSubmit, watch, formState } = useForm<CreateTagSchema>({
-    resolver: zodResolver(createTagSchema),
+    resolver: zodResolver(createTagSchema)
   })
 
   const slug = watch('title') 
-    ? getSlugFromString(watch('title')) 
-    : ''
+  ? getSlugFromString(watch('title')) 
+  : ''
 
   const { mutateAsync } = useMutation({
     mutationFn: async ({ title }: CreateTagSchema) => {
-      // delay 2s
+      // Delay de 2s
       await new Promise(resolve => setTimeout(resolve, 2000))
-
+  
       await fetch('http://localhost:3333/tags', {
         method: 'POST',
         body: JSON.stringify({
-          title,
+          title, 
           slug,
           amountOfVideos: 0,
-        }),
+        })
       })
-    },
+    }, 
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: ['get-tags'],
+        queryKey: ['get-tags']
       })
     }
   })
@@ -57,41 +48,50 @@ export function CreateTagForm() {
     await mutateAsync({ title })
   }
 
+  function getSlugFromString(input: string): string {
+    return  input
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .toLowerCase()
+      .replace(/[^\w\s]/g, '')
+      .replace(/\s+/g, '-');
+  }
+
   return (
-    <form onSubmit={handleSubmit(createTag)} className="w-full space-y-6">
-      <div className="space-y-2">
-        <label className="text-sm font-medium block" htmlFor="title">Tag name</label>
+    <form onSubmit={handleSubmit(createTag)} className='w-full space-y-6'>
+      <div className='space-y-2'>
+        <label className='text-sm font-medium block' htmlFor='title'>Tag name</label>
         <input 
           {...register('title')}
-          id="name" 
-          type="text" 
-          className="border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 w-full text-sm"
+          id='title'
+          type="text"
+          className='border border-zinc-800 rounded px-3 py-2.5 bg-zinc-800/50 w-full text-sm' 
         />
         {formState.errors?.title && (
-          <p className="text-sm text-red-400">{formState.errors.title.message}</p>
+          <p className='text-sm text-red-400'>{formState.errors.title.message}</p>
         )}
       </div>
 
-      <div className="space-y-2">
-        <label className="text-sm font-medium block" htmlFor="slug">Slug</label>
+      <div className='space-y-2'>
+        <label className='text-sm font-medium block' htmlFor="slug">Slug</label>
         <input 
-          id="slug" 
+          id='slug'
           type="text" 
-          readOnly 
+          readOnly
           value={slug}
-          className="border border-zinc-800 rounded-lg px-3 py-2 bg-zinc-800/50 w-full text-sm"
+          className='border border-zinc-800 rounded px-3 py-2.5 bg-zinc-800/50 w-full text-sm' 
         />
       </div>
 
-      <div className="flex items-center justify-end gap-2">
+      <div className='flex items-center justify-end gap-2'>
         <Dialog.Close asChild>
           <Button>
-            <X className="size-3" />
+            <X className='size-3'/>
             Cancel
           </Button>
         </Dialog.Close>
-        <Button disabled={formState.isSubmitting} className="bg-teal-400 text-teal-950" type="submit">
-          {formState.isSubmitting ? <Loader2 className="size-3 animate-spin" /> : <Check className="size-3" />}
+        <Button disabled={formState.isSubmitting} className='bg-teal-400 text-teal-950' type="submit">
+          {formState.isSubmitting ? <Loader2 className='size-3 animate-spin'/> : <Check className='size-3'/>}
           Save
         </Button>
       </div>
